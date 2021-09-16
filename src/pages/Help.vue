54c2242c164fe54765e5f0616b659227e22e26c2
Chanin Nusang <realchanin@gmail.com>
2021-09-16 14:27:04 +0700

    first commit

---
+++ src/pages/Help.vue
@@ -0,0 +1,14 @@
+<template>
+    <q-page class="q-pa-lg">
+    <h5 class="q-mt-none">Help</h5>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae aliquam ducimus sequi eaque perferendis fuga adipisci obcaecati, ut id. Quas, impedit aperiam dolore distinctio id soluta. Doloremque reiciendis quo amet?</p>
+    </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+
+})
+</script>
